f008ed62afd425cad02056cf76984a6c15e389d0
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-16 18:36:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay(musicians, instruments) {
 
 
 function johnLennonFacts(detail) {
-  var detail_exclamation = [];
+  let detail_exclamation = [];
   var i = 0;
   while (i < detail.length) {
     detail_exclamation.push(detail[i] + "!!!");
