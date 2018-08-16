ec63d4604c0156dde1c183495c35cd9c1183904c
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-16 18:09:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  const result = [];
+  for (var i = 0; i < musicians.length; i++) {
+    result.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return result;
+}
+
+
+function johnLennonFacts() {
+  var facts =
+}
