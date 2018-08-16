1b8f13b101d9ee4f0314ffe7705da5c58041e88a
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-16 18:25:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 
-function johnLennonFacts(){
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var newFacts = [];
-  var i = 0
-  while (i < facts.length) {
-    newFacts.push(facts[i] + "!!!")
-    i++;
+function johnLennonFacts(facts) {
+  var results = [];
+  var count = 0;
+  while (count < facts.length) {
+    results.push(facts[count] + "!!!");
+    count++;
   }
-  return newFacts;
-}
